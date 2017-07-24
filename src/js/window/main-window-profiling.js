8e7977970be44b21fccacff112fd494f6a6fa412
Eric Skogen <audionerd@users.noreply.github.com>
2017-07-24 11:09:02 -0500

    set a real brush

--- src/js/window/main-window-profiling.js
+++ src/js/window/main-window-profiling.js
@@ -1,11 +1,12 @@
 const { ipcRenderer, remote } = require('electron')
+const Color = require('color-js')
 
 const SketchPane = require('../sketch-pane')
 const StoryboarderSketchPane = require('./storyboarder-sketch-pane')
 
 let el = document.getElementById('storyboarder-sketch-pane')
-// el.style.width = '1600px'
-// el.style.height = '900px'
+el.style.width = '1600px'
+el.style.height = '900px'
 let rect = el.getBoundingClientRect()
 let elSize = [rect.width, rect.height].map(n => n * window.devicePixelRatio)
 
@@ -17,14 +18,25 @@ let storyboarderSketchPane = new StoryboarderSketchPane(
 // placeholder
 storyboarderSketchPane.toolbar = {
   getIsQuickErasing: () => false,
+  getBrushOptions: () => { return {} },
+  setIsQuickErasing: () => { },
   state: {
     transformMode: null
   },
-  emit: () => console.log('emit', arguments)
+  emit: () => {}
 }
 
+storyboarderSketchPane.setBrushTool('light-pencil', {
+  size: 7,
+  spacing: 0.25,
+  flow: 0.4,
+  hardness: 0.5,
+  opacity: 0.4,
+  color: Color('#121212')
+})
+
 const load = (event, filename, scriptData, locations, characters, boardSettings, currentPath) => {
-  setTimeout(() => remote.getCurrentWindow().show(), 200)
+  remote.getCurrentWindow().show()
 }
 
 ipcRenderer.on('load', load)
